2b642a325ce13fbda54d0c5adb8b95a4b5a01c57
sadafAsh <93827612+sadafAsh@users.noreply.github.com>
2022-06-15 10:45:17 +0100

    vedio 2 finished

---
+++ Tower Defence Game/Assets/Script/WaveSpawner.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WaveSpawner : MonoBehaviour
+{
+
+    public Transform enemyPrefab;
+    public Transform spawnPoint;
+    public float timeBetweenWaves = 5f;
+    private float countdown = 2f;
+    public Text waveCountdownText;
+    private int waveIndex = 0;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (countdown <= 0)
+        {
+           StartCoroutine(SpawnWave());
+            countdown = timeBetweenWaves;
+        }
+        countdown -= Time.deltaTime;
+        waveCountdownText.text =Mathf.Round(countdown).ToString();
+    }
+
+    IEnumerator SpawnWave()
+    {
+        waveIndex++;
+        for (int i = 0; i < waveIndex; i++)
+            
+        {
+
+            SpawnEnemy();
+            yield return new WaitForSeconds(0.5f);
+           
+        }
+        
+    }
+
+    void SpawnEnemy()
+    {
+        Instantiate(enemyPrefab,spawnPoint.position,spawnPoint.rotation);
+    }
+}
